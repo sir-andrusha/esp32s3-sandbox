56e93143b6368fa371b052b58b88206e32e1de3c
nichd147 <diniig147@gmail.com>
2025-01-17 23:29:59 +0400

    adds control of leds

--- restful_server/main/rest_server.c
+++ restful_server/main/rest_server.c
@@ -15,6 +15,10 @@
 #include "esp_vfs.h"
 #include "cJSON.h"
 
+#include "led_strip.h"
+#define BLINK_GPIO 48
+static led_strip_handle_t led_strip;
+
 static const char *REST_TAG = "esp-rest";
 #define REST_CHECK(a, str, goto_tag, ...)                                              \
     do                                                                                 \
@@ -135,8 +139,28 @@ static esp_err_t light_brightness_post_handler(httpd_req_t *req)
     int blue = cJSON_GetObjectItem(root, "blue")->valueint;
     ESP_LOGI(REST_TAG, "Light control: red = %d, green = %d, blue = %d", red, green, blue);
     cJSON_Delete(root);
+    led_strip_set_pixel(led_strip, 0, red, green, blue);
+    led_strip_refresh(led_strip);
     httpd_resp_sendstr(req, "Post control value successfully");
     return ESP_OK;
+}
+
+static void configure_led(void)
+{
+    ESP_LOGI(REST_TAG, "Example configured to blink addressable LED!");
+    /* LED strip initialization with the GPIO and pixels number*/
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = BLINK_GPIO,
+        .max_leds = 1, // at least one LED on board
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+        .flags.with_dma = false,
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+
+    /* Set all LED off to clear all pixels */
+    led_strip_clear(led_strip);
 }
 
 /* Simple handler for getting system handler */
@@ -170,6 +194,8 @@ static esp_err_t temperature_data_get_handler(httpd_req_t *req)
 
 esp_err_t start_rest_server(const char *base_path)
 {
+    configure_led();
+
     REST_CHECK(base_path, "wrong base path", err);
     rest_server_context_t *rest_context = calloc(1, sizeof(rest_server_context_t));
     REST_CHECK(rest_context, "No memory for rest context", err);
