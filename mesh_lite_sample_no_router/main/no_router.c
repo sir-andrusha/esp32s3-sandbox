4dee450394ce8dec60092d20016f055187489e9e
nichd147 <diniig147@gmail.com>
2025-01-19 02:09:55 +0400

    mesh lite no route: adds new site

--- mesh_lite_sample_no_router/main/no_router.c
+++ mesh_lite_sample_no_router/main/no_router.c
@@ -18,18 +18,18 @@
 #include "esp_bridge.h"
 #include "esp_mesh_lite.h"
 
-static const char *TAG = "no_router";
+static const char* TAG = "no_router";
 
 /**
  * @brief Timed printing system information
  */
 static void print_system_info_timercb(TimerHandle_t timer)
 {
-    uint8_t primary                 = 0;
-    uint8_t sta_mac[6]              = {0};
-    wifi_ap_record_t ap_info        = {0};
-    wifi_second_chan_t second       = 0;
-    wifi_sta_list_t wifi_sta_list   = {0x0};
+    uint8_t primary = 0;
+    uint8_t sta_mac[6] = { 0 };
+    wifi_ap_record_t ap_info = { 0 };
+    wifi_second_chan_t second = 0;
+    wifi_sta_list_t wifi_sta_list = { 0x0 };
 
     if (esp_mesh_lite_get_level() > 1) {
         esp_wifi_sta_get_ap_info(&ap_info);
@@ -39,21 +39,21 @@ static void print_system_info_timercb(TimerHandle_t timer)
     esp_wifi_get_channel(&primary, &second);
 
     ESP_LOGI(TAG, "System information, channel: %d, layer: %d, self mac: " MACSTR ", parent bssid: " MACSTR
-             ", parent rssi: %d, free heap: %"PRIu32"", primary,
-             esp_mesh_lite_get_level(), MAC2STR(sta_mac), MAC2STR(ap_info.bssid),
-             (ap_info.rssi != 0 ? ap_info.rssi : -120), esp_get_free_heap_size());
+        ", parent rssi: %d, free heap: %"PRIu32"", primary,
+        esp_mesh_lite_get_level(), MAC2STR(sta_mac), MAC2STR(ap_info.bssid),
+        (ap_info.rssi != 0 ? ap_info.rssi : -120), esp_get_free_heap_size());
 
     for (int i = 0; i < wifi_sta_list.num; i++) {
         ESP_LOGI(TAG, "Child mac: " MACSTR, MAC2STR(wifi_sta_list.sta[i].mac));
     }
 
     uint32_t size = 0;
-    const node_info_list_t *node = esp_mesh_lite_get_nodes_list(&size);
+    const node_info_list_t* node = esp_mesh_lite_get_nodes_list(&size);
     printf("MeshLite nodes %ld:\r\n", size);
     for (uint32_t loop = 0; (loop < size) && (node != NULL); loop++) {
         struct in_addr ip_struct;
         ip_struct.s_addr = node->node->ip_addr;
-        printf("%ld: %d, "MACSTR", %s\r\n" , loop + 1, node->node->level, MAC2STR(node->node->mac_addr), inet_ntoa(ip_struct));
+        printf("%ld: %d, "MACSTR", %s\r\n", loop + 1, node->node->level, MAC2STR(node->node->mac_addr), inet_ntoa(ip_struct));
         node = node->next;
     }
 }
@@ -80,8 +80,8 @@ static void wifi_init(void)
     esp_bridge_wifi_set_config(WIFI_IF_STA, &wifi_config);
 
     // Softap
-    snprintf((char *)wifi_config.ap.ssid, sizeof(wifi_config.ap.ssid), "%s", CONFIG_BRIDGE_SOFTAP_SSID);
-    strlcpy((char *)wifi_config.ap.password, CONFIG_BRIDGE_SOFTAP_PASSWORD, sizeof(wifi_config.ap.password));
+    snprintf((char*)wifi_config.ap.ssid, sizeof(wifi_config.ap.ssid), "%s", CONFIG_BRIDGE_SOFTAP_SSID);
+    strlcpy((char*)wifi_config.ap.password, CONFIG_BRIDGE_SOFTAP_PASSWORD, sizeof(wifi_config.ap.password));
     wifi_config.ap.channel = CONFIG_MESH_CHANNEL;
     esp_bridge_wifi_set_config(WIFI_IF_AP, &wifi_config);
 }
@@ -134,12 +134,11 @@ static void configure_led(void)
     blue = 254;
 }
 
-static void light_refresh(void *arg) {
+static void light_refresh(void* arg) {
     while (1) {
         uint8_t level = esp_mesh_lite_get_level();
 
-        for(int i = 0; i<level; i++){
-            ESP_LOGI(TAG, "clear: i = %d", i);
+        for (int i = 0; i < level; i++) {
             led_strip_set_pixel(led_strip, 0, red, green, blue);
             led_strip_refresh(led_strip);
             vTaskDelay(500 / portTICK_PERIOD_MS);
@@ -169,11 +168,11 @@ typedef struct rest_server_context {
 } rest_server_context_t;
 
 /* Simple handler for light brightness control */
-static esp_err_t light_brightness_post_handler(httpd_req_t *req)
+static esp_err_t light_brightness_post_handler(httpd_req_t* req)
 {
     int total_len = req->content_len;
     int cur_len = 0;
-    char *buf = ((rest_server_context_t *)(req->user_ctx))->scratch;
+    char* buf = ((rest_server_context_t*)(req->user_ctx))->scratch;
     int received = 0;
     if (total_len >= SCRATCH_BUFSIZE) {
         /* Respond with 500 Internal Server Error */
@@ -191,7 +190,7 @@ static esp_err_t light_brightness_post_handler(httpd_req_t *req)
     }
     buf[total_len] = '\0';
 
-    cJSON *root = cJSON_Parse(buf);
+    cJSON* root = cJSON_Parse(buf);
     red = cJSON_GetObjectItem(root, "red")->valueint;
     green = cJSON_GetObjectItem(root, "green")->valueint;
     blue = cJSON_GetObjectItem(root, "blue")->valueint;
@@ -229,35 +228,41 @@ static esp_err_t light_brightness_post_handler(httpd_req_t *req)
 #define CHECK_FILE_EXTENSION(filename, ext) (strcasecmp(&filename[strlen(filename) - strlen(ext)], ext) == 0)
 
 /* Set HTTP response content type according to file extension */
-static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filepath)
+static esp_err_t set_content_type_from_file(httpd_req_t* req, const char* filepath)
 {
-    const char *type = "text/plain";
+    const char* type = "text/plain";
     if (CHECK_FILE_EXTENSION(filepath, ".html")) {
         type = "text/html";
-    } else if (CHECK_FILE_EXTENSION(filepath, ".js")) {
+    }
+    else if (CHECK_FILE_EXTENSION(filepath, ".js")) {
         type = "application/javascript";
-    } else if (CHECK_FILE_EXTENSION(filepath, ".css")) {
+    }
+    else if (CHECK_FILE_EXTENSION(filepath, ".css")) {
         type = "text/css";
-    } else if (CHECK_FILE_EXTENSION(filepath, ".png")) {
+    }
+    else if (CHECK_FILE_EXTENSION(filepath, ".png")) {
         type = "image/png";
-    } else if (CHECK_FILE_EXTENSION(filepath, ".ico")) {
+    }
+    else if (CHECK_FILE_EXTENSION(filepath, ".ico")) {
         type = "image/x-icon";
-    } else if (CHECK_FILE_EXTENSION(filepath, ".svg")) {
+    }
+    else if (CHECK_FILE_EXTENSION(filepath, ".svg")) {
         type = "text/xml";
     }
     return httpd_resp_set_type(req, type);
 }
 
 /* Send HTTP response with the contents of the requested file */
-static esp_err_t rest_common_get_handler(httpd_req_t *req)
+static esp_err_t rest_common_get_handler(httpd_req_t* req)
 {
     char filepath[FILE_PATH_MAX];
 
-    rest_server_context_t *rest_context = (rest_server_context_t *)req->user_ctx;
+    rest_server_context_t* rest_context = (rest_server_context_t*)req->user_ctx;
     strlcpy(filepath, rest_context->base_path, sizeof(filepath));
     if (req->uri[strlen(req->uri) - 1] == '/') {
         strlcat(filepath, "/index.html", sizeof(filepath));
-    } else {
+    }
+    else {
         strlcat(filepath, req->uri, sizeof(filepath));
     }
     int fd = open(filepath, O_RDONLY, 0);
@@ -270,14 +275,15 @@ static esp_err_t rest_common_get_handler(httpd_req_t *req)
 
     set_content_type_from_file(req, filepath);
 
-    char *chunk = rest_context->scratch;
+    char* chunk = rest_context->scratch;
     ssize_t read_bytes;
     do {
         /* Read file in chunks into the scratch buffer */
         read_bytes = read(fd, chunk, SCRATCH_BUFSIZE);
         if (read_bytes == -1) {
             ESP_LOGE(TAG, "Failed to read file : %s", filepath);
-        } else if (read_bytes > 0) {
+        }
+        else if (read_bytes > 0) {
             /* Send the buffer contents as HTTP response chunk */
             if (httpd_resp_send_chunk(req, chunk, read_bytes) != ESP_OK) {
                 close(fd);
@@ -299,35 +305,79 @@ static esp_err_t rest_common_get_handler(httpd_req_t *req)
 }
 
 /* Simple handler for getting system handler */
-static esp_err_t system_info_get_handler(httpd_req_t *req)
+static esp_err_t system_info_get_handler(httpd_req_t* req)
 {
     httpd_resp_set_type(req, "application/json");
-    cJSON *root = cJSON_CreateObject();
+    cJSON* root = cJSON_CreateObject();
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
     cJSON_AddStringToObject(root, "version", IDF_VER);
     cJSON_AddNumberToObject(root, "cores", chip_info.cores);
-    const char *sys_info = cJSON_Print(root);
+    const char* sys_info = cJSON_Print(root);
     httpd_resp_sendstr(req, sys_info);
-    free((void *)sys_info);
+    free((void*)sys_info);
+    cJSON_Delete(root);
+    return ESP_OK;
+}
+
+static esp_err_t names_get_handler(httpd_req_t* req)
+{
+    httpd_resp_set_type(req, "application/json");
+    cJSON* root = cJSON_CreateArray();
+
+    uint32_t size = 0;
+    const node_info_list_t* node = esp_mesh_lite_get_nodes_list(&size);
+    printf("MeshLite nodes %ld:\r\n", size);
+    for (uint32_t loop = 0; (loop < size) && (node != NULL); loop++) {
+
+        uint8_t lvl = node->node->level;
+
+        struct in_addr ip_struct;
+        if (lvl == 1) {
+            esp_netif_ip_info_t sta_ip;
+            memset(&sta_ip, 0x0, sizeof(esp_netif_ip_info_t));
+            esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_AP_DEF"), &sta_ip);
+            ip_struct.s_addr = sta_ip.ip.addr;
+        }
+        else {
+            ip_struct.s_addr = node->node->ip_addr;
+        }
+
+        char mac_str[19] = { 0 };
+        sprintf(mac_str, MACSTR, MAC2STR(node->node->mac_addr));
+
+        cJSON* item = cJSON_CreateObject();
+        cJSON_AddStringToObject(item, "mac", mac_str);
+        cJSON_AddStringToObject(item, "ip", inet_ntoa(ip_struct));
+        cJSON_AddNumberToObject(item, "level", lvl);
+        cJSON_AddNumberToObject(item, "color", 0);
+
+        cJSON_AddItemToArray(root, item);
+
+        node = node->next;
+    }
+
+    const char* names = cJSON_Print(root);
+    httpd_resp_sendstr(req, names);
+    free((void*)names);
     cJSON_Delete(root);
     return ESP_OK;
 }
 
 /* Simple handler for getting temperature data */
-static esp_err_t temperature_data_get_handler(httpd_req_t *req)
+static esp_err_t temperature_data_get_handler(httpd_req_t* req)
 {
     httpd_resp_set_type(req, "application/json");
-    cJSON *root = cJSON_CreateObject();
+    cJSON* root = cJSON_CreateObject();
     cJSON_AddNumberToObject(root, "raw", esp_random() % 20);
-    const char *sys_info = cJSON_Print(root);
+    const char* sys_info = cJSON_Print(root);
     httpd_resp_sendstr(req, sys_info);
-    free((void *)sys_info);
+    free((void*)sys_info);
     cJSON_Delete(root);
     return ESP_OK;
 }
 
-void init_handlers(httpd_handle_t server, rest_server_context_t *rest_context)
+void init_handlers(httpd_handle_t server, rest_server_context_t* rest_context)
 {
     /* URI handler for fetching system info */
     httpd_uri_t system_info_get_uri = {
@@ -346,15 +396,23 @@ void init_handlers(httpd_handle_t server, rest_server_context_t *rest_context)
         .user_ctx = rest_context
     };
     httpd_register_uri_handler(server, &temperature_data_get_uri);
-    
-        httpd_uri_t light_brightness_post_uri = {
-        .uri = "/api/v1/light/brightness",
-        .method = HTTP_POST,
-        .handler = light_brightness_post_handler,
+
+    httpd_uri_t light_brightness_post_uri = {
+    .uri = "/api/v1/light/brightness",
+    .method = HTTP_POST,
+    .handler = light_brightness_post_handler,
+    .user_ctx = rest_context
+    };
+
+    httpd_register_uri_handler(server, &light_brightness_post_uri);
+
+    httpd_uri_t names_get_uri = {
+        .uri = "/api/v1/names",
+        .method = HTTP_GET,
+        .handler = names_get_handler,
         .user_ctx = rest_context
     };
-    
-    httpd_register_uri_handler(server, &light_brightness_post_uri);
+    httpd_register_uri_handler(server, &names_get_uri);
 
     /* URI handler for getting web server files */
     httpd_uri_t common_get_uri = {
@@ -380,9 +438,11 @@ esp_err_t init_fs(void)
     if (ret != ESP_OK) {
         if (ret == ESP_FAIL) {
             ESP_LOGE(TAG, "Failed to mount or format filesystem");
-        } else if (ret == ESP_ERR_NOT_FOUND) {
+        }
+        else if (ret == ESP_ERR_NOT_FOUND) {
             ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-        } else {
+        }
+        else {
             ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
         }
         return ESP_FAIL;
@@ -392,7 +452,8 @@ esp_err_t init_fs(void)
     ret = esp_spiffs_info(NULL, &total, &used);
     if (ret != ESP_OK) {
         ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
-    } else {
+    }
+    else {
         ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
     }
     return ESP_OK;
@@ -412,14 +473,14 @@ static void initialise_mdns(void)
     };
 
     ESP_ERROR_CHECK(mdns_service_add("SwarmDronWebServer", "_http", "_tcp", 80, serviceTxtData,
-                                     sizeof(serviceTxtData) / sizeof(serviceTxtData[0])));
+        sizeof(serviceTxtData) / sizeof(serviceTxtData[0])));
 }
 #else
 
 #include <sys/param.h>
 
 /* An HTTP POST handler */
-static esp_err_t echo_post_handler(httpd_req_t *req)
+static esp_err_t echo_post_handler(httpd_req_t* req)
 {
     char buf[100];
     int ret, remaining = req->content_len;
@@ -427,7 +488,7 @@ static esp_err_t echo_post_handler(httpd_req_t *req)
     while (remaining > 0) {
         /* Read the data for the request */
         if ((ret = httpd_req_recv(req, buf,
-                        MIN(remaining, sizeof(buf)))) <= 0) {
+            MIN(remaining, sizeof(buf)))) <= 0) {
             if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
                 /* Retry receiving if timeout occurred */
                 continue;
@@ -450,13 +511,13 @@ static esp_err_t echo_post_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
-void init_handlers(httpd_handle_t server, rest_server_context_t *rest_context)
+void init_handlers(httpd_handle_t server, rest_server_context_t* rest_context)
 {
     httpd_uri_t echo = {
-        .uri       = "/echo",
-        .method    = HTTP_POST,
-        .handler   = echo_post_handler,
-        .user_ctx  = NULL
+        .uri = "/echo",
+        .method = HTTP_POST,
+        .handler = echo_post_handler,
+        .user_ctx = NULL
     };
 
     httpd_register_uri_handler(server, &echo);
@@ -474,12 +535,12 @@ void init_handlers(httpd_handle_t server, rest_server_context_t *rest_context)
 
 #endif
 
-httpd_handle_t start_rest_server(const char *base_path)
+httpd_handle_t start_rest_server(const char* base_path)
 {
 
     REST_CHECK(base_path, "wrong base path", err);
 
-    rest_server_context_t *rest_context = calloc(1, sizeof(rest_server_context_t));
+    rest_server_context_t* rest_context = calloc(1, sizeof(rest_server_context_t));
     REST_CHECK(rest_context, "No memory for rest context", err);
     strlcpy(rest_context->base_path, base_path, sizeof(rest_context->base_path));
 
@@ -506,24 +567,24 @@ static esp_err_t stop_rest_server(httpd_handle_t server)
     return httpd_stop(server);
 }
 
-static void disconnect_handler(void* arg, esp_event_base_t event_base,
-                               int32_t event_id, void* event_data)
-{
-    httpd_handle_t* server = (httpd_handle_t*) arg;
+static void disconnect_handler(void* arg, esp_event_base_t event_base,    int32_t event_id, void* event_data){
+    httpd_handle_t* server = (httpd_handle_t*)arg;
     if (*server) {
         ESP_LOGI(TAG, "Stopping webserver");
         if (stop_rest_server(*server) == ESP_OK) {
             *server = NULL;
-        } else {
+        }
+        else {
             ESP_LOGE(TAG, "Failed to stop http server");
         }
     }
 }
 
-static void connect_handler(void* arg, esp_event_base_t event_base,
-                            int32_t event_id, void* event_data)
-{
-    httpd_handle_t* server = (httpd_handle_t*) arg;
+static void connect_handler(void* arg, esp_event_base_t event_base,    int32_t event_id, void* event_data){
+    ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
+    ESP_LOGI(TAG, "got event IP_EVENT_STA_GOT_IP, ip:" IPSTR, IP2STR(&event->ip_info.ip));
+
+    httpd_handle_t* server = (httpd_handle_t*)arg;
     if (*server == NULL) {
         ESP_LOGI(TAG, "Starting webserver");
         *server = start_rest_server(CONFIG_EXAMPLE_WEB_MOUNT_POINT);
@@ -570,7 +631,7 @@ void app_main()
     xTaskCreate(light_refresh, "light_refresh", 4 * 1024, NULL, 5, NULL);
 
     TimerHandle_t timer = xTimerCreate("print_system_info", 10000 / portTICK_PERIOD_MS,
-                                       true, NULL, print_system_info_timercb);
+        true, NULL, print_system_info_timercb);
     xTimerStart(timer, 0);
 
 #if CONFIG_MESH_ROOT
@@ -582,7 +643,7 @@ void app_main()
 #endif
 
     static httpd_handle_t server = NULL;
-    
+
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
 
